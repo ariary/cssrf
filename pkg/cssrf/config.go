a56d02047570843deb8b65411548616e660b8f98
Ariary <ariary9.2@hotmail.fr>
2022-06-02 10:37:17 +0200

    Update config.go

--- pkg/cssrf/config.go
+++ pkg/cssrf/config.go
@@ -1,9 +1,11 @@
 package cssrf
 
 type Config struct {
-	Length      int
-	Port        string
-	ExternalUrl string
-	Charset     string
-	Template    string
+	Length             int
+	Port               string
+	ExternalUrl        string
+	Charset            string
+	BackgroundTemplate string
+	ImportTemplate     string
+	Channels           []chan string
 }
